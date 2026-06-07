47d09055ea867b7f6144f42d7c4c90e16eab1a6f
developergyu <keg1621@gmail.com>
2026-06-07 22:00:22 +0900

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -26,7 +26,7 @@ headers_krx = {
 ############################################
 # 2. 날짜 설정
 ############################################
-today = datetime.today() - timedelta(days=3)
+today = datetime.today()
 target_dt = pd.to_datetime(today.strftime('%Y-%m-%d'))
 
 # 전일 (단순 D-1, 필요시 영업일 계산 로직 추가 가능)
